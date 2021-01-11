4c3a3fc7436b3a826c7ba69b6f9ebacac67c366a
Elias Croze <elias.croze@symphony.com>
2021-01-11 14:56:57 +0100

    Proxy usage

--- login/__init__.py
+++ login/__init__.py
@@ -1,5 +1,6 @@
 import asyncio
 
+import urllib3
 from jose import jwt
 
 import datetime
@@ -8,27 +9,12 @@ import datetime
 def urllib_post(token):
     from openapi_client_urllib import ApiClient, Configuration, AuthenticateRequest, AuthenticationApi
     request = AuthenticateRequest(token=token)
-    configuration = Configuration(host="https://devx1.symphony.com/login")
+    configuration = Configuration(host="https://develop2.symphony.com/login")
+    configuration.proxy = "http://127.0.0.1:3128"  # for proxy
+    configuration.proxy_headers = urllib3.util.make_headers(proxy_basic_auth='proxyuser:passw0rd') # for proxy basic authentication
 
     api = AuthenticationApi(ApiClient(configuration=configuration))
     post = api.pubkey_authenticate_post(authenticate_request=request)
-    print(post)
-
-
-def aiohttp_post():
-    from openapi_client_aio.api.authentication_api import AuthenticationApi
-    from openapi_client_aio.api_client import ApiClient
-    from openapi_client_aio.configuration import Configuration
-    from openapi_client_aio.models.authenticate_request import AuthenticateRequest
-
-    request = AuthenticateRequest(token=TOKEN)
-    configuration = Configuration(host="https://devx1.symphony.com/login")
-    api = AuthenticationApi(ApiClient(configuration=configuration))
-
-    loop = asyncio.get_event_loop()
-    post = loop.run_until_complete(api.pubkey_authenticate_post(authenticate_request=request, async_req=True).get())
-    loop.close()
-
     print(post)
 
 
@@ -50,6 +36,4 @@ def create_jwt():
 
 
 if __name__ == "__main__":
-    encoded = create_jwt()
-    print(encoded)
-    urllib_post(encoded)
+    urllib_post(create_jwt())
