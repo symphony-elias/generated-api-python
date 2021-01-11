979a2c606f795bd4bdbe3bb8f2a2dbc7c7b2248e
Elias Croze <elias.croze@symphony.com>
2021-01-11 15:37:48 +0100

    Agent api

--- agent_urllib/api/datafeed_api.py
+++ agent_urllib/api/datafeed_api.py
@@ -17,8 +17,8 @@ import re  # noqa: F401
 # python 2 and python 3 compatibility library
 import six
 
-from openapi_client.api_client import ApiClient
-from openapi_client.exceptions import (  # noqa: F401
+from agent_urllib.api_client import ApiClient
+from agent_urllib.exceptions import (  # noqa: F401
     ApiTypeError,
     ApiValueError
 )
--- agent_urllib/api_client.py
+++ agent_urllib/api_client.py
@@ -24,10 +24,10 @@ import tempfile
 import six
 from six.moves.urllib.parse import quote
 
-from openapi_client.configuration import Configuration
-import openapi_client.models
-from openapi_client import rest
-from openapi_client.exceptions import ApiValueError, ApiException
+from agent_urllib.configuration import Configuration
+import agent_urllib.models
+from agent_urllib import rest
+from agent_urllib.exceptions import ApiValueError, ApiException
 
 
 class ApiClient(object):
@@ -305,7 +305,7 @@ class ApiClient(object):
             if klass in self.NATIVE_TYPES_MAPPING:
                 klass = self.NATIVE_TYPES_MAPPING[klass]
             else:
-                klass = getattr(openapi_client.models, klass)
+                klass = getattr(agent_urllib.models, klass)
 
         if klass in self.PRIMITIVE_TYPES:
             return self.__deserialize_primitive(data, klass)
