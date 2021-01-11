acb8bb071eadd2479d028729627068d5280aeaf2
Elias Croze <elias.croze@symphony.com>
2021-01-11 15:34:32 +0100

    added agent api

---
+++ agent_urllib/api/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.attachments_api import AttachmentsApi
+from openapi_client.api.audit_trail_api import AuditTrailApi
+from openapi_client.api.dlp_policies_and_dictionary_management_api import DLPPoliciesAndDictionaryManagementApi
+from openapi_client.api.datafeed_api import DatafeedApi
+from openapi_client.api.messages_api import MessagesApi
+from openapi_client.api.share_api import ShareApi
+from openapi_client.api.signals_api import SignalsApi
+from openapi_client.api.system_api import SystemApi
+from openapi_client.api.util_api import UtilApi
+from openapi_client.api.violations_api import ViolationsApi
